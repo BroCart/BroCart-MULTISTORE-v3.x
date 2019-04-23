75d7d1f4b9ed78cd94df203ec452957512e55cbb
ALarik <baco@mail.ua>
2019-04-23 22:14:58 +0300

    add dir to UKR lacalization

---
+++ admin/language/uk-ua/marketplace/api.php
@@ -0,0 +1,18 @@
+<?php
+// Заголовок
+$_['heading_title']    = 'API Магазину';
+
+// Текстовые
+$_['text_success']     = 'Налаштування успішно змінено!';
+$_['text_signup']      = 'Будь ласка, введіть свій OpenCart API котрий вказано <a href="https://www.opencart.com/index.php?route=account/store" target="_blank" class="alert-link">тут</a>.';
+
+// Поля ввода
+$_['entry_username']   = 'Ім\'я користувача';
+$_['entry_secret']     = 'Пароль';
+
+// Ошибки
+$_['error_permission'] = 'У Вас нема прав для керування модулем!';
+$_['error_username']   = 'Ім\'я користувача необхідно!';
+$_['error_secret']     = 'Пароль необходний!';
+
+
