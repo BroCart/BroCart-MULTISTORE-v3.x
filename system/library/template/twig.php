d49cc2f000f5e234d71feaa1ee40805cc500f407
CMS BroCart <artur.coder@brocart.net>
2019-05-16 15:12:19 +0300

    upd twig adaptor

--- system/library/template/twig.php
+++ system/library/template/twig.php
@@ -15,27 +15,41 @@ final class Twig {
 		$this->data[$key] = $value;
 	}
 	
-	public function render($template, $cache = false) {
-		// specify where to look for templates
-		$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);
-
-		// initialize Twig environment
-		$config = array('autoescape' => false);
-
-		if ($cache) {
-			$config['cache'] = DIR_CACHE;
-		}
-
-		$this->twig = new \Twig_Environment($loader, $config);
+	public function render($template, $cache = false, $templater) {
+		$file = DIR_TEMPLATE . $template . '.tpl';
 		
-		try {
-			// load template
-			$template = $this->twig->loadTemplate($template . '.twig');
+		if ($templater != 'default') {
+			$file = str_replace('default', $templater, $file);
+		}
+		
+		if (is_file($file)) {			
+			extract($this->data);
+			ob_start();
+			require($file);
+			return ob_get_clean();
+		} else {
+			// specify where to look for templates
+			$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);	
 			
-			return $template->render($this->data);
-		} catch (Exception $e) {
-			trigger_error('Error: Could not load template ' . $template . '!');
-			exit();	
-		}	
+			// initialize Twig environment
+			$config = array('autoescape' => false);
+			
+			// initialize Twig environment
+			if ($cache) {
+				$config['cache'] = DIR_CACHE;
+			}
+			
+			$this->twig = new \Twig_Environment($loader, $config);
+			
+			try {
+				// load template
+				$template = $this->twig->loadTemplate($template . '.twig');
+				
+				return $template->render($this->data);
+			} catch (Exception $e) {
+				trigger_error('Error: Could not load template ' . $template . '!');
+				exit();	
+			}	
+		}
 	}	
 }
