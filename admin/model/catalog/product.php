84f62d3d4d44aacde75b8b349d4e45a682580ab7
CMS BroCart <artur.coder@brocart.net>
2019-05-30 15:28:17 +0300

    Update product.php

--- admin/model/catalog/product.php
+++ admin/model/catalog/product.php
@@ -89,9 +89,10 @@ class ModelCatalogProduct extends Model {
 		}
 		
 		if (isset($data['main_category_id']) && $data['main_category_id'] > 0) {
+			$this->db->query("DELETE FROM " . DB_PREFIX . "product_to_category WHERE product_id = '" . (int)$product_id . "' AND category_id = '" . (int)$data['main_category_id'] . "'");
 			$this->db->query("INSERT INTO " . DB_PREFIX . "product_to_category SET product_id = '" . (int)$product_id . "', category_id = '" . (int)$data['main_category_id'] . "', main_category = 1");
 		} elseif (isset($data['product_category'][0])) {
-			$this->db->query("INSERT INTO " . DB_PREFIX . "product_to_category SET product_id = '" . (int)$product_id . "', category_id = '" . (int)$data['product_category'][0] . "', main_category = 1");
+			$this->db->query("UPDATE " . DB_PREFIX . "product_to_category SET main_category = 1 WHERE product_id = '" . (int)$product_id . "' AND category_id = '" . (int)$data['product_category'][0] . "'");
 		}
 
 		if (isset($data['product_filter'])) {
